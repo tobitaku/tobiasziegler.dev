e034d81dfb32e5455e92152d1285d408dda0b2ea
Tobias <tobias.c.ziegler@gmail.com>
2023-10-30 12:42:14 +0100

    Clean up

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,15 +4,17 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const posts = await getCollection("posts");
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Tobias Ziegler | Blog',
+    description: 'Software-Entwickler mit 4 Jahren Web-Erfahrung. Derzeit arbeite ich mit React und TypeScript.',
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
+      author: post.data.author,
       link: `/posts/${post.slug}/`,
+      categories: post.data.tags
     })),
-    customData: `<language>en-us</language>`,
+    customData: `<language>de-de</language>`,
   })
 }
